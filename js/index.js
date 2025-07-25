3058b1becc36ed29264793d96621a8b2982b9989
Rafael Alves <168144782+rafaelAlvesQaTester@users.noreply.github.com>
2025-07-25 00:00:16 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Inserindo um comentário simples
+document.getElementById("texto").innerHTML = "Meu programa em <b>Javascript</b>!";
+
+/*
+comentário para varias linhas
+Teste 1
+Teste 2
+Teste 3
+Teste n...
+*/
